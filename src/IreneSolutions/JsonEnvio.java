4b5e303019ac0b01658d79b818faa817b6952d3c
ivan <ivitorrito@gmail.com>
2025-10-05 13:55:28 +0200

    Casa

---
+++ src/IreneSolutions/JsonEnvio.java
@@ -0,0 +1,151 @@
+package IreneSolutions;
+
+
+public class JsonEnvio {
+    String ServiceKey;
+    String Status;
+    String InvoiceType;
+    String InvoiceID;
+    String InvoiceDate;
+    String SellerID;
+    String CompanyName;
+    String RelatedPartyID;
+    String RelatedPartyName;
+    String Text;
+    String TaxItems;
+    String TaxScheme;
+    String TaxType;
+    String TaxRate;
+    String TaxBase;
+    String TaxAmount;
+
+    public String getServiceKey() {
+        return ServiceKey;
+    }
+
+    public void setServiceKey(String ServiceKey) {
+        this.ServiceKey = ServiceKey;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public String getInvoiceType() {
+        return InvoiceType;
+    }
+
+    public void setInvoiceType(String InvoiceType) {
+        this.InvoiceType = InvoiceType;
+    }
+
+    public String getInvoiceID() {
+        return InvoiceID;
+    }
+
+    public void setInvoiceID(String InvoiceID) {
+        this.InvoiceID = InvoiceID;
+    }
+
+    public String getInvoiceDate() {
+        return InvoiceDate;
+    }
+
+    public void setInvoiceDate(String InvoiceDate) {
+        this.InvoiceDate = InvoiceDate;
+    }
+
+    public String getSellerID() {
+        return SellerID;
+    }
+
+    public void setSellerID(String SellerID) {
+        this.SellerID = SellerID;
+    }
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String CompanyName) {
+        this.CompanyName = CompanyName;
+    }
+
+    public String getRelatedPartyID() {
+        return RelatedPartyID;
+    }
+
+    public void setRelatedPartyID(String RelatedPartyID) {
+        this.RelatedPartyID = RelatedPartyID;
+    }
+
+    public String getRelatedPartyName() {
+        return RelatedPartyName;
+    }
+
+    public void setRelatedPartyName(String RelatedPartyName) {
+        this.RelatedPartyName = RelatedPartyName;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String Text) {
+        this.Text = Text;
+    }
+
+    public String getTaxItems() {
+        return TaxItems;
+    }
+
+    public void setTaxItems(String TaxItems) {
+        this.TaxItems = TaxItems;
+    }
+
+    public String getTaxScheme() {
+        return TaxScheme;
+    }
+
+    public void setTaxScheme(String TaxScheme) {
+        this.TaxScheme = TaxScheme;
+    }
+
+    public String getTaxType() {
+        return TaxType;
+    }
+
+    public void setTaxType(String TaxType) {
+        this.TaxType = TaxType;
+    }
+
+    public String getTaxRate() {
+        return TaxRate;
+    }
+
+    public void setTaxRate(String TaxRate) {
+        this.TaxRate = TaxRate;
+    }
+
+    public String getTaxBase() {
+        return TaxBase;
+    }
+
+    public void setTaxBase(String TaxBase) {
+        this.TaxBase = TaxBase;
+    }
+
+    public String getTaxAmount() {
+        return TaxAmount;
+    }
+
+    public void setTaxAmount(String TaxAmount) {
+        this.TaxAmount = TaxAmount;
+    }
+    
+    
+}
